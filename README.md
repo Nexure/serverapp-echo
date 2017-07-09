ba4a872173acf720d0a7cc55b6196e96eb3034a0
Anthony Comtois <anthony.comtois@gmail.com>
2017-07-09 18:42:00 +0100

    update readme

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
-docker-compose up 
+## bootstrap application Framework Golang Echo + Cassandra 
 
+Start server + database, hot reload of the golang server.
+
+```
+docker-compose up
+```
+
+Improvements:
+- Add jsonapi - https://github.com/manyminds/api2go
+- Add socketio feature
+- Add auth - JWT feature
+- RBAC - Casbin Auth Middleware
 
--- src/libs/components/users/users.go
+++ src/libs/components/users/users.go
@@ -1,34 +1,34 @@
 package users
 
 import (
-  "net/http"
-  "github.com/labstack/echo"
-  "github.com/rewiko/app/libs/cassandra"
-  "github.com/gocql/gocql"
+	"net/http"
+
+	"github.com/gocql/gocql"
+	"github.com/labstack/echo"
+	"github.com/rewiko/app/libs/cassandra"
 )
 
-
 type User struct {
-	ID        gocql.UUID `json:"id"`
-	Text     string `json:"text"`
-	TimeLine  string `json:timeline"`
+	ID       gocql.UUID `json:"id"`
+	Text     string     `json:"text"`
+	TimeLine string     `json:"time_line"`
 }
 
 func List(c echo.Context) error {
 
- var userList []User
-  m := map[string]interface{}{}
+	var userList []User
+	m := map[string]interface{}{}
 
-  query := "SELECT * from example.tweet;"
-  iterable := cassandra.Session.Query(query).Iter()
-  for iterable.MapScan(m) {
-    userList = append(userList, User{
-      ID: m["id"].(gocql.UUID),
-      Text: m["text"].(string),
-      TimeLine: m["timeline"].(string),
-    })
-    m = map[string]interface{}{}
-  }
+	query := "SELECT * from example.tweet;"
+	iterable := cassandra.Session.Query(query).Iter()
+	for iterable.MapScan(m) {
+		userList = append(userList, User{
+			ID:       m["id"].(gocql.UUID),
+			Text:     m["text"].(string),
+			TimeLine: m["timeline"].(string),
+		})
+		m = map[string]interface{}{}
+	}
 
-  return c.JSON(http.StatusOK, userList)
+	return c.JSON(http.StatusOK, userList)
 }
